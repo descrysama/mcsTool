2cca4702ceaaf42e10c9870807150e6e78554c51
Desc <63260058+descrysama@users.noreply.github.com>
2023-06-26 16:38:33 +0200

    update console.log

--- functions/fetchDataMobilax.js
+++ functions/fetchDataMobilax.js
@@ -39,7 +39,7 @@ const fetchDataMobilax = async(token, array, mobilax_apis) => {
 const fetchEntireStore = async(token, mobilax_apis) => {
     let mobilaxentireproducts = [];
 
-    mobilax_apis.forEach(async(api, index) => {
+    for(const api of mobilax_apis) {
         try {
             const response = await fetch(api.url, {
                 headers: {
@@ -51,7 +51,7 @@ const fetchEntireStore = async(token, mobilax_apis) => {
         } catch(e) {
             console.log("Erreur fetch mobilax :" + e)
         }
-    })
+    }
 
     return mobilaxentireproducts
 }
